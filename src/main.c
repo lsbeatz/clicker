4b658ffc535e2d60ed8a7238485d30dd8efe289e
lsbeatz <dlatjdqo34@gmail.com>
2025-07-19 21:51:29 +0900

    feat(ui): Enhance button UI with borders, hover effects, and dynamic colors

--- src/main.c
+++ src/main.c
@@ -24,7 +24,7 @@ void render_text(SDL_Renderer *renderer, TTF_Font *font, const char *text, int x
 	}
 	SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
 	if (!texture) {
-		SDL_Log("Unable to create text texture: %s", SDL_GetError());
+		SDL_Log("Unable to create text texture: %s", TTF_GetError());
 		SDL_FreeSurface(surface);
 		return;
 	}
@@ -48,19 +48,23 @@ int main(int argc, char **argv)
 											  SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-	TTF_Font *font_large = TTF_OpenFont("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 24);
-	TTF_Font *font_small = TTF_OpenFont("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 12);
+	TTF_Font *font_large = TTF_OpenFont("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 28);
+	TTF_Font *font_small = TTF_OpenFont("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 14);
 
 	// Game state
-	long long money			= 0;
+	long long money			= 1000;
 	int money_per_click		= 1;
 	int money_per_second	= 0;
 	Uint32 last_second_tick = SDL_GetTicks();
 
 	// Colors
-	SDL_Color color_white = { 255, 255, 255, 255 };
-	SDL_Color color_black = { 0, 0, 0, 255 };
-	SDL_Color color_grey  = { 100, 100, 100, 255 };
+	SDL_Color color_white			  = { 255, 255, 255, 255 };
+	SDL_Color color_black			  = { 0, 0, 0, 255 };
+	SDL_Color color_grey			  = { 50, 50, 50, 255 };
+	SDL_Color color_grey_hover		  = { 80, 80, 80, 255 };
+	SDL_Color color_border			  = { 20, 20, 20, 255 };
+	SDL_Color color_cost_affordable	  = { 100, 255, 100, 255 }; // Light Green
+	SDL_Color color_cost_unaffordable = { 255, 100, 100, 255 }; // Light Red
 
 	// Initialize Upgrades
 	Upgrade upgrades[4];
@@ -68,8 +72,8 @@ int main(int argc, char **argv)
 	for (int i = 0; i < 4; i++) {
 		upgrades[i].name   = names[i];
 		upgrades[i].level  = 0;
-		upgrades[i].rect.w = 100;
-		upgrades[i].rect.h = 50;
+		upgrades[i].rect.w = 150;
+		upgrades[i].rect.h = 60;
 		upgrades[i].rect.x = SCREEN_WIDTH - (4 - i) * (upgrades[i].rect.w + 10) - 10;
 		upgrades[i].rect.y = SCREEN_HEIGHT - upgrades[i].rect.h - 10;
 	}
@@ -116,9 +120,9 @@ int main(int argc, char **argv)
 								upgrades[i].cost *= upgrades[i].cost_multiplier;
 
 								if (i == 0) { // Q - Click Power
-									money_per_click = 1 + upgrades[i].level;
+									money_per_click = 1 + upgrades[i].level * 2;
 								} else if (i == 1) { // W - Passive Income
-									money_per_second = upgrades[i].level;
+									money_per_second = upgrades[i].level * 1;
 								}
 							}
 						}
@@ -143,29 +147,47 @@ int main(int argc, char **argv)
 		render_text(renderer, font_large, buffer, 10, 10, color_black);
 
 		snprintf(buffer, sizeof(buffer), "Click Power: $%d", money_per_click);
-		render_text(renderer, font_small, buffer, 10, 40, color_black);
+		render_text(renderer, font_small, buffer, 10, 50, color_black);
 
 		snprintf(buffer, sizeof(buffer), "$/sec: %d", money_per_second);
-		render_text(renderer, font_small, buffer, 10, 55, color_black);
+		render_text(renderer, font_small, buffer, 10, 70, color_black);
+
+		// Get mouse state for hover effect
+		int mouse_x, mouse_y;
+		SDL_GetMouseState(&mouse_x, &mouse_y);
+		SDL_Point mouse_point = { mouse_x, mouse_y };
 
 		// Render upgrade buttons
 		for (int i = 0; i < 4; i++) {
-			// Draw button background
-			SDL_SetRenderDrawColor(renderer, color_grey.r, color_grey.g, color_grey.b, color_grey.a);
+			SDL_Color current_bg_color = color_grey;
+			// Check for hover
+			if (SDL_PointInRect(&mouse_point, &upgrades[i].rect)) {
+				current_bg_color = color_grey_hover;
+			}
+
+			// Draw button border
+			SDL_SetRenderDrawColor(renderer, color_border.r, color_border.g, color_border.b, color_border.a);
 			SDL_RenderFillRect(renderer, &upgrades[i].rect);
 
+			// Draw button background
+			SDL_Rect bg_rect = { upgrades[i].rect.x + 2, upgrades[i].rect.y + 2, upgrades[i].rect.w - 4,
+								 upgrades[i].rect.h - 4 };
+			SDL_SetRenderDrawColor(renderer, current_bg_color.r, current_bg_color.g, current_bg_color.b,
+								   current_bg_color.a);
+			SDL_RenderFillRect(renderer, &bg_rect);
+
 			// Draw main name (Q, W, E, R)
-			render_text(renderer, font_large, upgrades[i].name, upgrades[i].rect.x + (upgrades[i].rect.w / 2) - 8,
-						upgrades[i].rect.y + (upgrades[i].rect.h / 2) - 12, color_white);
+			render_text(renderer, font_large, upgrades[i].name, upgrades[i].rect.x + 20,
+						upgrades[i].rect.y + (upgrades[i].rect.h / 2) - 16, color_white);
 
 			// Draw Level
 			snprintf(buffer, sizeof(buffer), "Lv.%d", upgrades[i].level);
-			render_text(renderer, font_small, buffer, upgrades[i].rect.x + 5, upgrades[i].rect.y + 5, color_white);
+			render_text(renderer, font_small, buffer, upgrades[i].rect.x + 80, upgrades[i].rect.y + 12, color_white);
 
 			// Draw Cost
+			SDL_Color cost_color = (money >= upgrades[i].cost) ? color_cost_affordable : color_cost_unaffordable;
 			snprintf(buffer, sizeof(buffer), "$%lld", upgrades[i].cost);
-			render_text(renderer, font_small, buffer, upgrades[i].rect.x + 5,
-						upgrades[i].rect.y + upgrades[i].rect.h - 20, color_white);
+			render_text(renderer, font_small, buffer, upgrades[i].rect.x + 80, upgrades[i].rect.y + 32, cost_color);
 		}
 
 		SDL_RenderPresent(renderer);
